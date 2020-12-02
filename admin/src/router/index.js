ddd54e13d6e7a6ce950878dd0408452ea6620f94
period9149 <yxx9149@gmail.com>
2020-12-02 18:00:48 +0800

    12345

---
+++ admin/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    children:[
+      {
+        path: '/about',
+        name: 'About',
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      }
+    ]
+  },
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
